a98934be0bce03721ca68ab3d14fae52c506129d
Dk <45588778+dipen8699@users.noreply.github.com>
2022-03-22 01:06:56 +0530

    [Add]Project in list

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,25 +1,25 @@
 const Projects = [
-    {
-        id: 1,
-        name: 'DoorPost',
-        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-        info: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-        language: ['python', 'Html', 'Css', 'Js'],
-    },
-    {
-        id: 2,
-        name: 'Ai base quality assurance',
-        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-        info: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-        language: ['python'],
-    },
-    {
-        id: 3,
-        name: 'Crypto transfer',
-        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-        info: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-        language: ['Solidity', 'React', 'Web3.js'],
-    },
+	{
+		id: 1,
+		name: 'Amazon-Clone',
+		image: './assets/amazon.png',
+		info: 'Amazon clone is basically a project which I created with my partner to learn about reactjs, firebase. I am using react language as a frontend and firebase as a backend to store data in a real-time database. you can use this app to purchase products and add this product to the card. we are using 2 different payment systems which make this project more unique. Payment Systems --> 1- using stripe payment system API for the user to make a payment proceed with multiple cards such as Visa, Mastercard, express and more,2- we use web3 payment system in this app which is not only comfortable for the user but also give them a reliable payment option to make a payment fast and cheaper price with the help of blockchain. we also use firebase authentication for users. Auth Feature --> 1- user can register with their email and password and username,2- user can log in with 3 different login systems (with email and password, with G-mail, with Facebook),3- user can also reset their password as well as if the user will forget their password in future than easily they can change their password with verification mail',
+		language: ['ReactJs', 'Firebase', 'Solidity', 'Web3Js', 'Hardhat'],
+	},
+	{
+		id: 2,
+		name: 'Ai base quality assurance',
+		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+		info: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+		language: ['python'],
+	},
+	{
+		id: 3,
+		name: 'Crypto transfer',
+		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+		info: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+		language: ['Solidity', 'React', 'Web3.js'],
+	},
 ]
 
 export default Projects
