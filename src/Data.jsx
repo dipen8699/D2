e10cca8cdca1baf0368df83479938ea8f2ba9181
Dk <45588778+dipen8699@users.noreply.github.com>
2022-03-22 01:21:14 +0530

    [Add]Project in list

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,7 +2,7 @@ const Projects = [
 	{
 		id: 1,
 		name: 'Amazon-Clone',
-		image: './assets/amazon_background.png',
+		image: './assets/amazon_background.jpg',
 		info: 'Amazon clone is basically a project which I created with my partner to learn about reactjs, firebase. I am using react language as a frontend and firebase as a backend to store data in a real-time database. you can use this app to purchase products and add this product to the card. we are using 2 different payment systems which make this project more unique. Payment Systems --> 1- using stripe payment system API for the user to make a payment proceed with multiple cards such as Visa, Mastercard, express and more,2- we use web3 payment system in this app which is not only comfortable for the user but also give them a reliable payment option to make a payment fast and cheaper price with the help of blockchain. we also use firebase authentication for users. Auth Feature --> 1- user can register with their email and password and username,2- user can log in with 3 different login systems (with email and password, with G-mail, with Facebook),3- user can also reset their password as well as if the user will forget their password in future than easily they can change their password with verification mail',
 		github: 'https://github.com/dipen8699/amazon-clone',
 		live: 'https://d2-amazon-clone.netlify.app/',
